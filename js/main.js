51b3c44922e70d2f597b206fff99f9cb12c52e78
thevonzhimanen <32675847+thevonzhimanen@users.noreply.github.com>
2021-02-28 13:07:55 -0500

    execute function for fade-out
    also added:
    -different timestamp format
    -different dataBuildings csv

--- js/main.js
+++ js/main.js
@@ -25,7 +25,10 @@ d3.csv("data/buildingBlock.csv", function(result){
     dataBuildings = result;
 });
 
-
+function Execute(){
+    document.getElementById("mondrian").className = "fade_out";
+    document.getElementById("chartArea").className = "fade_in";
+    document.getElementById("canvasOverlay").style.display="none";
 
 //request data from API    
 function fetchdata() {
@@ -63,7 +66,7 @@ function fetchdata() {
 function writedata(data) {
     var t = new Date();
     var timeid = t.getTime();
-    var timeStamp = t.toISOString().replace(/:/g, "-").replace(".", "-");
+    var timeStamp = t.toString();
     var filename = "img-" + timeid;
     //how to take the svg/xml structure and simply write it to firebase?
     //var dataSVG = (new XMLSerializer()).serializeToString(document.getElementById("chartArea").getElementsByTagName("svg").item(0));
@@ -72,7 +75,7 @@ function writedata(data) {
 
     db.ref('images/' + filename).set({
         id: filename,
-        dataSVG: "dataSVG",
+        dataSVG: "null",
         dataJSON: data,
         time: timeStamp
     });
@@ -251,6 +254,7 @@ $(document).ready(function(){
         document.getElementById("mondrian").style.display = "none";
         var chartArea = document.getElementById("chartArea");
         var dataOnce = snapshot.val();
+        console.log(dataOnce);
 
         //the JSONData variable is the JSON format of the last request. Can use that to draw a canvas while you wait for the next request to come in.
         var dataJSONLast = Object.values(dataOnce)[Object.keys(dataOnce).length - 1].dataJSON;
@@ -290,12 +294,13 @@ $(document).ready(function(){
 
             //archiveTitle contains the title text of the svg:
             var titleElement = document.createElement("p");
-            var archiveTitle = document.createTextNode(dataOnce[key].time + ":");
+            var archiveTitle = document.createTextNode(dataOnce[key].time.substr(0, 24) + ":");
             titleElement.appendChild(archiveTitle);
 
             //archiveCanvas contains the svg:
             var archiveCanvas = document.createElement("div");
             archiveCanvas.id = dataOnce[key].time;
+            archiveElement.className = "archiveCanvas";
             var dataJSON = dataOnce[key].dataJSON;
             drawSVG(dataJSON, archiveCanvas, .25);
 
@@ -310,21 +315,4 @@ $(document).ready(function(){
     });
 });
     
- function writedata(data) {
-        var t = new Date();
-        var timeid = t.getTime();
-        var timeStamp = t.toISOString().replace(/:/g, "-").replace(".", "-");
-        var filename = "img-" + timeid;
-        //how to take the svg/xml structure and simply write it to firebase?
-        //var dataSVG = (new XMLSerializer()).serializeToString(document.getElementById("chartArea").getElementsByTagName("svg").item(0));
-
-        console.log(timeid, timeStamp, filename, data);
-
-        db.ref('images/' + filename).set({
-            id: filename,
-            dataSVG: "dataSVG",
-            dataJSON: data,
-            time: timeStamp
-            });
 }
-
